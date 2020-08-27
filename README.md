036ca8f792df274a94d823ec82682243ec457f0c
Daniel Ramos de Carvalho <danielramosbh74@gmail.com>
2020-08-27 19:17:13 -0300

    first upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node test package
+
+GitHub Package Registry Node example
+
+A test repository and package for working with NodeJS and the GitHub Package Registry
+
+# Instructions
+
+1. x
+2. x
---
+++ index.js
@@ -0,0 +1,11 @@
+// It's just a very simple example of how can we use Github Packages with NodeJS
+// Maybe it's the most simple example. At least that's what I'm trying to do...
+
+function simple () {
+
+    return console.log ('If you can read this it means that this function was successfully exported and imported from this module.');
+
+};
+
+res = simple();
+console.log (res);
